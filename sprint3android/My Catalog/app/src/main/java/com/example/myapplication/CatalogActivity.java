e098032d255747afc134820a5db75157a839fcbd
dcurrasv25 <dcurrasv25@fpcoruna.afundacion.org>
2025-11-10 13:05:56 +0100

    Ejercicio 2

---
+++ sprint3android/My Catalog/app/src/main/java/com/example/myapplication/CatalogActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        Button navigateButton = findViewById(R.id.button_navigate);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
